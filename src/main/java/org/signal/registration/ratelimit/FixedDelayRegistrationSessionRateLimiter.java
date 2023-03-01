ca064f10f55cce72a173c28bbeaa81a9581de68c
Jon Chambers <jon@signal.org>
2023-03-01 11:50:55 -0500

    Fall back to the current time until all sessions have a creation timestamp

--- src/main/java/org/signal/registration/ratelimit/FixedDelayRegistrationSessionRateLimiter.java
+++ src/main/java/org/signal/registration/ratelimit/FixedDelayRegistrationSessionRateLimiter.java
@@ -63,8 +63,12 @@ public abstract class FixedDelayRegistrationSessionRateLimiter implements RateLi
     final Optional<Instant> maybeNextAction;
 
     if (attempts == 0) {
-      // If the caller has never attempted this action before, they may do so immediately
-      maybeNextAction = Optional.of(Instant.ofEpochMilli(session.getCreatedEpochMillis()));
+      // If the caller has never attempted this action before, they may do so immediately. To support
+      // deployment, fall back to the current time for session creation time if no creation time is set; once deployment
+      // is complete and all sessions have a non-zero creation time, we can use session creation time unconditionally.
+      maybeNextAction = Optional.of(session.getCreatedEpochMillis() > 0
+          ? Instant.ofEpochMilli(session.getCreatedEpochMillis())
+          : clock.instant());
     } else if (attempts <= delays.size()) {
       maybeNextAction = Optional.of(maybeLastAttempt
           .orElseThrow(() -> new IllegalStateException("Last attempt must be present if attempt count is non-zero"))
