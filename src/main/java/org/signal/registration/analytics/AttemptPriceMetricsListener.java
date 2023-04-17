80746d6e9d88fde2177ba3a840359ab64460ec68
Jon Chambers <jon@signal.org>
2023-04-17 16:23:45 -0400

    Count the number of attempts included in cost metrics

--- src/main/java/org/signal/registration/analytics/AttemptPriceMetricsListener.java
+++ src/main/java/org/signal/registration/analytics/AttemptPriceMetricsListener.java
@@ -7,6 +7,7 @@ package org.signal.registration.analytics;
 
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import io.micronaut.configuration.metrics.annotation.RequiresMetrics;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.event.ApplicationEventListener;
@@ -32,7 +33,11 @@ class AttemptPriceMetricsListener implements ApplicationEventListener<AttemptAna
   private static final Currency USD = Currency.getInstance("USD");
   private static final BigDecimal ONE_MILLION = new BigDecimal("1e6");
 
-  private static final String COUNTER_NAME = MetricsUtil.name(AttemptPriceMetricsListener.class, "attemptPriceMicros");
+  private static final String ATTEMPT_COUNTER_NAME =
+      MetricsUtil.name(AttemptPriceMetricsListener.class, "attemptsAnalyzed");
+
+  private static final String PRICE_COUNTER_NAME =
+      MetricsUtil.name(AttemptPriceMetricsListener.class, "attemptPriceMicros");
 
   private static final Logger logger = LoggerFactory.getLogger(AttemptPriceMetricsListener.class);
 
@@ -44,14 +49,15 @@ class AttemptPriceMetricsListener implements ApplicationEventListener<AttemptAna
   public void onApplicationEvent(final AttemptAnalyzedEvent event) {
     event.attemptAnalysis().price().ifPresent(price -> {
       if (USD.equals(price.currency())) {
-        meterRegistry.counter(COUNTER_NAME,
-                MetricsUtil.SENDER_TAG_NAME, event.attemptPendingAnalysis().getSenderName(),
-                MetricsUtil.CLIENT_TYPE_TAG_NAME, MetricsUtil.getClientTypeTagValue(event.attemptPendingAnalysis().getClientType()),
-                MetricsUtil.TRANSPORT_TAG_NAME, MetricsUtil.getMessageTransportTagValue(event.attemptPendingAnalysis().getMessageTransport()),
-                MetricsUtil.VERIFIED_TAG_NAME, String.valueOf(event.attemptPendingAnalysis().getVerified()),
-                MetricsUtil.REGION_CODE_TAG_NAME, event.attemptPendingAnalysis().getRegion(),
-                MetricsUtil.COUNTRY_CODE_TAG_NAME, String.valueOf(PhoneNumberUtil.getInstance().getCountryCodeForRegion(event.attemptPendingAnalysis().getRegion())))
-            .increment(price.amount().multiply(ONE_MILLION).longValue());
+        final Tags tags = Tags.of(MetricsUtil.SENDER_TAG_NAME, event.attemptPendingAnalysis().getSenderName(),
+            MetricsUtil.CLIENT_TYPE_TAG_NAME, MetricsUtil.getClientTypeTagValue(event.attemptPendingAnalysis().getClientType()),
+            MetricsUtil.TRANSPORT_TAG_NAME, MetricsUtil.getMessageTransportTagValue(event.attemptPendingAnalysis().getMessageTransport()),
+            MetricsUtil.VERIFIED_TAG_NAME, String.valueOf(event.attemptPendingAnalysis().getVerified()),
+            MetricsUtil.REGION_CODE_TAG_NAME, event.attemptPendingAnalysis().getRegion(),
+            MetricsUtil.COUNTRY_CODE_TAG_NAME, String.valueOf(PhoneNumberUtil.getInstance().getCountryCodeForRegion(event.attemptPendingAnalysis().getRegion())));
+
+        meterRegistry.counter(ATTEMPT_COUNTER_NAME, tags).increment();
+        meterRegistry.counter(PRICE_COUNTER_NAME, tags).increment(price.amount().multiply(ONE_MILLION).longValue());
       } else {
         logger.warn("Price provided in non-USD currency ({}) by {}", price.currency(), event.attemptPendingAnalysis().getSenderName());
       }
